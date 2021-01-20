9d8be20e0f78bc9dac0f0a9e358ccf7315038798
Website1301 <77741759+Website1301@users.noreply.github.com>
2021-01-21 00:14:03 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,54 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cash Plus</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<script type="text/javascript" src="http://me.kis.v2.scr.kaspersky-labs.com/FD126C42-EBFA-4E12-B309-BB3FDD723AC1/main.js?attr=AVuHrBnzutWXvXc1SNuK2i1q84qe31XCLB3bJBqRwPzy8xyzrjWksPsMdV9bkUdlVfI7qKxdhgKxaWOabgycfg" charset="UTF-8"></script><script type="text/javascript" src="http://me.kis.v2.scr.kaspersky-labs.com/FD126C42-EBFA-4E12-B309-BB3FDD723AC1/main.js?attr=_AywJsj01zr4eJKEoNhkwQq1F1ZE1mF5bbmwRTUsLNNME237NA6Gfz3Za7VsV3NQ7nEp4BeyOcGrS0KsgDCf7w" charset="UTF-8"></script><style>
+.mySlides {display:none;}
+body {font-family: Times New Roman;}
+.navbar {
+  width: 100%;
+ background-color:#e6ffff;
+  overflow: auto;
+}
+.navbar a{
+   float:right;
+   padding:16px;
+   color:#000066;
+   text-decoration:none;
+   font-size: 20px;
+}
+.navbar a:hover{
+   background-color:#0000cc:
+}
+
+@media screen and (max-width:200px)
+.navbar a{
+   float:none;
+   display:block;
+ }
+}
+</style>
+</head>
+<style>
+body{
+  background-image: url('https://media.istockphoto.com/photos/stack-of-coins-on-wood-desks-and-loft-wall-backgrounds-with-sunlight-picture-id1254701717?k=6&m=1254701717&s=170667a&w=0&h=Ol0Axcr6ewYCX9XrIZnDIQF7g8v3ATNZSChxyGDLgmU=');
+  background-repeat: no-repeat;
+  background-attachment:fixed;
+  background-size:100% 100%;
+}
+</style>
+<body>
+<h4 style ="color:Black;text-align:left;font-family: Times New Roman;">Cash Plus<i class="fa fa-university"></i></h1>
+<div class="navbar">
+ <a href="#"><b>Transaction History</b></a>
+<a href="transfermoney.php"><b>Transfer Money</b></a>
+ <a href="createuser.php"><b>Create User</b></a>
+<a href="index.php"><b>Home</b></a>
+</div>  
+</body>
+</html>
