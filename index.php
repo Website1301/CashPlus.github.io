4a3286edaa62ddd75874d7a2d5c52d3ab9c48185
Website1301 <77741759+Website1301@users.noreply.github.com>
2021-01-21 13:33:10 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,169 @@
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Cash Plus</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta content="text/html; charset=iso-8859-2" http-equiv="Content-Type">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+.mySlides {display:none;}
+body {font-family: Times New Roman;}
+.navbar {
+  width: 100%;
+ background-color:#e6ffff;
+  overflow: auto;
+}
+.navbar a{
+   float:right;
+   padding:16px;
+   color:#000066;
+   text-decoration:none;
+   font-size: 20px;
+}
+.navbar a:hover{
+   background-color:#0000cc:
+}
+
+@media screen and (max-width:200px)
+.navbar a{
+   float:none;
+   display:block;
+ }
+}
+</style>
+</head>
+<style>
+body{
+  background-image: url('https://media.istockphoto.com/photos/stack-of-coins-on-wood-desks-and-loft-wall-backgrounds-with-sunlight-picture-id1254701717?k=6&m=1254701717&s=170667a&w=0&h=Ol0Axcr6ewYCX9XrIZnDIQF7g8v3ATNZSChxyGDLgmU=');
+  background-repeat: no-repeat;
+  background-attachment:fixed;
+  background-size:100% 100%;
+}
+</style>
+<body>
+<h4 style ="color:Black;text-align:left;font-family: Times New Roman;">Cash Plus<i class="fa fa-university"></i></h1>
+<div class="navbar">
+ <a href="transaction-history.php"><b>Transaction History</b></a>
+<a href="transfermoney.php"><b>Transfer Money</b></a>
+ <a href="createuser.php"><b>Create User</b></a>
+<a href="index.php"><b>Home</b></a>
+</div>  
+<style>
+body, html {
+  height: 100%;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.hero-image {
+  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)), url("Bank.png");
+  height: 40%;
+  background-position: left;
+  background-repeat: no-repeat;
+  background-size: auto;
+  position: relative;
+}
+
+.hero-text {
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white;
+}
+</style>
+</head>
+<body>
+<div class="hero-image">
+  <div class="hero-text">
+    <p> <h1 style ="color:AntiqueWhite;text-align:center;font-family: Times New Roman;"><u><b>Cash Plus</u></b></h1>
+  <h2 style ="color:AntiqueWhite;text-align:center;font-family: Times New Roman;"><u>Welcome to E-Bank</u></h1>  
+</p>
+  </div><br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<style>
+div.gallery {
+  margin: 100px;
+  border: 5px solid #660000;
+  float: left;
+  width: 300px;
+  height: 200px;
+}
+
+div.gallery:hover {
+  border: 1 px solid #660000;
+}
+
+div.gallery img {
+  width: 100%;
+  height: auto; 
+}
+div.desc {
+  padding: 28px;
+  text-align: center;
+  font-family:Times New Roman;
+  color:#000066;
+
+}
+
+</style>
+</head>
+<body>
+
+
+<div class="gallery">
+  <a target="_blank" href="https://dev.graphicsfuel.com/wp-content/uploads/elementor/thumbs/user-icon-nb3gdk4h6wenoqr1whh59f596vrt6vt06og82y8cxo.jpg
+">
+    <img src="https://dev.graphicsfuel.com/wp-content/uploads/elementor/thumbs/user-icon-nb3gdk4h6wenoqr1whh59f596vrt6vt06og82y8cxo.jpg
+" alt="User" style="width:300px; height:200px;">
+  </a>
+  <div class="desc"><a href="createuser.php"><button style=background-color:#ffeecc;><b>Create a User</button></b></div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="https://i0.wp.com/www.iedunote.com/img/10964/what-is-transactions-types-of-accounting-transactions.jpg?fit=1000%2C791&quality=100&ssl=1
+">
+    <img src="https://i0.wp.com/www.iedunote.com/img/10964/what-is-transactions-types-of-accounting-transactions.jpg?fit=1000%2C791&quality=100&ssl=1
+" alt="Make a Transaction"style="width:300px; height:200px;">
+  </a>
+  <div class="desc" ><a href="transfermoney.php"><button style=background-color:#ffeecc;><b>Make a Transaction</b></button></div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="transactionhistory.jpg">
+    <img src="transactionhistory.jpg" alt="Transaction History" style="width:300px; height:200px;"> 
+</a>
+  <div class="desc"><a href="transaction-history.php"> <button style=background-color:#ffeecc;><b>Transaction History</b></button></div>
+</div>
+</body>
+<br>
+<br>
+<br>
+<br>
+
+ <footer>
+  <p><b><h4 style ="color:White;text-align:center;font-family: Times New Roman;">Made by -Akanksha Bharne <br>Under The Sparks Foundation</h4></b></p>
+  </footer>
+
+
+</body>
+</html>
+
+
+
+
